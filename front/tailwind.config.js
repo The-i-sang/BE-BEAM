f33908b7bbb79557ca51973db6aae42a09527bfa
Yoo Seungmin <dnjfht@gmail.com>
2023-11-09 19:03:41 +0900

    Activity page : 반응형 UI 작업 완료

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "0calc": "calc(100% - 32px)",
+        "1calc": "calc(100% - 164px)",
+        "2calc": "calc(100% - 140px)",
+      },
+    },
   },
   corePlugins: {
     aspectRatio: true,
