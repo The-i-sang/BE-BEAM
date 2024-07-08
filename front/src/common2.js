b5bfb3ba9e20ec83ba2f0373245f212666d09384
Yoo Seungmin <dnjfht@gmail.com>
2024-07-08 20:16:13 +0900

    chore : Modify navigation paths.

--- front/src/common2.js
+++ front/src/common2.js
@@ -1,14 +1,14 @@
 // 프로젝트에서 공통으로 사용하는 변수들을 모아둔 곳.
 
 export const AppPage = {
-  home: "/",
+  meeting: "/",
+  meetingDetail: "/meeting/detail",
+  home: "/brand",
   auth: "/auth",
   mypage: "mypage",
   search: "/search",
   toolkit: "/toolkit",
   toolkitDetail: "/toolkit/detail",
-  meeting: "/meeting",
-  meetingDetail: "/meeting/detail",
   applyForm: "/applyForm",
   userInfoModify: "/mypage/userInfoModify",
   userProfileModify: "/mypage/userProfileModify",
