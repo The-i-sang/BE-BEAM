9635c6ad9305900451b978c7a28cab554b50b8d4
Yoo Seungmin <dnjfht@gmail.com>
2024-08-19 14:43:38 +0900

    Deleting backend files.

--- back/toolkit/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Toolkit, ToolkitImage, ToolkitMainCategory, ToolkitSubCategory
-
-admin.site.register(Toolkit)
-admin.site.register(ToolkitImage)
-admin.site.register(ToolkitMainCategory)
-admin.site.register(ToolkitSubCategory)
